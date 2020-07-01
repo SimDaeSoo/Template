45b56a14d85a8c0f7ac2290f5c950da55eb6edbb
bigdeal <bigdeal@smartstudy.co.kr>
2020-07-01 19:34:17 +0900

    default port 변경.

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,7 +3,7 @@ import SocketServer from './classes/SocketServer';
 async function start(): Promise<void> {
     const server: SocketServer = new SocketServer();
     await server.initialize();
-    server.open(3000);
+    server.open(3030);
 }
 
 start();
