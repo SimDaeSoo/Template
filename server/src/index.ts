d4d5428141fb29d6a91f9c21510e48c716a2d110
bigdeal <bigdeal@smartstudy.co.kr>
2020-07-01 12:17:06 +0900

    socket server initialize.

---
+++ server/src/index.ts
@@ -0,0 +1,9 @@
+import SocketServer from './classes/SocketServer';
+
+async function start(): Promise<void> {
+    const server: SocketServer = new SocketServer();
+    await server.initialize();
+    server.open(3000);
+}
+
+start();
