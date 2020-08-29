a71a118daa412c88291cd3f70c4d2111d8d7690a
bigdeal <bigdeal@smartstudy.co.kr>
2020-08-29 21:14:01 +0900

    Scroll Bar customizing, Save Button add.

--- nextjs/components/ToastEditor.jsx
+++ nextjs/components/ToastEditor.jsx
@@ -3,25 +3,34 @@ import { Editor } from '@toast-ui/react-editor';
 import colorSyntaxPlugin from "@toast-ui/editor-plugin-color-syntax";
 import hljs from "highlight.js";
 import codeSyntaxHighlight from "@toast-ui/editor-plugin-code-syntax-highlight";
+import { Button } from 'antd';
+import { SaveOutlined } from '@ant-design/icons';
 
 class ToastEditor extends React.Component {
     render() {
         return (
-            <Editor
-                initialValue=""
-                previewStyle="vertical"
-                width='100%'
-                height='100%'
-                initialEditType="wysiwyg"
-                useCommandShortcut={true}
-                usageStatistics={false}
-                previewHighlight={false}
-                plugins={[[codeSyntaxHighlight, { hljs }], colorSyntaxPlugin]}
-            // hooks={{
-            //     addImageBlobHook: (blob, callback) => {
-            //     }
-            // }}
-            />
+            <div style={{ width: '100%', height: '100%', position: 'relative' }}>
+                <Editor
+                    initialValue=""
+                    previewStyle="vertical"
+                    width='100%'
+                    height='100%'
+                    initialEditType="wysiwyg"
+                    useCommandShortcut={true}
+                    usageStatistics={false}
+                    previewHighlight={false}
+                    plugins={[[codeSyntaxHighlight, { hljs }], colorSyntaxPlugin]}
+                // hooks={{
+                //     addImageBlobHook: (blob, callback) => {
+                //     }
+                // }}
+                />
+                <div style={{ position: 'absolute', bottom: '32px', right: '12px' }}>
+                    <Button type="primary" icon={<SaveOutlined />}>
+                        Save
+                    </Button>
+                </div>
+            </div>
         )
     }
 }
